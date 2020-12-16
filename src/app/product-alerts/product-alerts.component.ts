9c16bf6722515c76bcb84befd3c6723d6ed41fd2
mabah-lab <maliouh@yahoo.fr>
2020-12-16 12:45:51 +0100

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { from } from 'rxjs';
+import { Output, EventEmitter} from '@angular/core'
 
 @Component({
   selector: 'app-product-alerts',
