2ca663e0fcd1af23b9913bc04aa1f9c8c51d7096
mabah-lab <maliouh@yahoo.fr>
2020-12-17 11:29:47 +0100

    purchase

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,13 +10,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
   constructor(
-    private cartService: CartService
-  ) { }
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) { 
+    this.checkoutForm= this.formBuilder.group({
+      name: '',
+      adresse: ''
+    });
+  }
 
   ngOnInit() {
     this.items= this.cartService.getItems();
   }
+  onSubmit(customerData){
+    this.items=this.cartService.clearCart();
+    this.checkoutForm.reset();
+    console.warn('Your order has been submitted', customerData);
+
+  }
+
 
 
 }
